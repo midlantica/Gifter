18a577e1bed0e445a6dc2a02e82e81746e422a62
drewharper <druharper@gmail.com>
2020-09-30 12:06:28 -0500

    Add project files.

---
+++ Gifter/Models/UserProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gifter.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+    }
+}
---
+++ Gifter/Repositories/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    internal interface IPostRepository
+    {
+        void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
+        void GetWithComments(int id);
+        object GetAll();
+        object GetAllWithComments(int id);
+        object GetById(int id);
+    }
+}
