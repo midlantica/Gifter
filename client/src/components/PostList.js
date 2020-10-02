022c317252f300469f079c035eef2177fb08ba6d
drewharper <druharper@gmail.com>
2020-10-02 16:45:33 -0500

    everything present, but form not working.

---
+++ client/src/components/PostList.js
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from "react";
+import { PostContext } from "../providers/PostProvider";
+//import Post from "./Post";
+import PostForm from "./PostForm";
+
+const PostList = () => {
+  const { posts, getAllPosts } = useContext(PostContext);
+
+  useEffect(() => {
+    getAllPosts();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="cards-column">
+          {posts.map((post) => (
+            <PostForm key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostList;
